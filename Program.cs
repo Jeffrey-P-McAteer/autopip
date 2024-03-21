aea82e4b779ec95a96ee0dd997727956760993e6
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-03-21 07:44:01 -0400

    simple sitecustomize PoC

--- Program.cs
+++ Program.cs
@@ -1,15 +1,33 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace AutoPip
 {
     class Program
     {
-        static void Main(string[] args)
+        // Returns string path to sitecusomize module
+        public static string CreatePython_sitecustomize() {
+          string tmp_dir = Path.GetTempPath();
+          string tmp_sitecustomize_py = tmp_dir+Path.DirectorySeparatorChar+"sitecustomize.py";
+          File.WriteAllText(tmp_sitecustomize_py, @"
+print('Hello sitecustomize!')
+
+");
+          return tmp_sitecustomize_py;
+        }
+        public static void Main(string[] args)
         {
+            string sitecustomize_location = CreatePython_sitecustomize();
+            Console.WriteLine($"sitecustomize_location = {sitecustomize_location}");
+
             ProcessStartInfo psi = new ProcessStartInfo("python");
             // psi.ArgumentList.Add("chaos.py");
             psi.Arguments = "chaos.py";
+            psi.EnvironmentVariables.Add("PYTHONPATH",
+                Environment.GetEnvironmentVariable("PYTHONPATH")+Path.PathSeparator+
+                Path.GetDirectoryName(sitecustomize_location)
+            );
 
             var p = Process.Start(psi);
             p.WaitForExit();
