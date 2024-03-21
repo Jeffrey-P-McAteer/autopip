5b9ebc88f57f9afa9e37d1998c71837d9a0169d5
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-03-21 09:27:39 -0400

    Added a _TON_ of power by waking the stack to the import line and reading comment as pip args

--- Program.cs
+++ Program.cs
@@ -8,12 +8,93 @@ namespace AutoPip
     {
         // Returns string path to sitecusomize module
         public static string CreatePython_sitecustomize() {
-          string tmp_dir = Path.GetTempPath();
+          string tmp_dir = Path.GetTempPath().TrimEnd(Path.DirectorySeparatorChar);
           string tmp_sitecustomize_py = tmp_dir+Path.DirectorySeparatorChar+"sitecustomize.py";
           File.WriteAllText(tmp_sitecustomize_py, @"
-print('Hello sitecustomize!')
+import sys
+import os
+import importlib
+import importlib.abc
+import importlib.machinery
 
-");
+import subprocess
+import tempfile
+import traceback
+
+# Does not load packages; instead, we install this as the first
+# item in sys.meta_path and we install everything that
+# can't be loaded using importlib.import_module() - careful of the recursion!
+class AutoPipMetaFinder(importlib.abc.MetaPathFinder):
+  def __init__(self):
+    self.currently_recursing = False
+    self.module_install_path = os.path.join(
+      tempfile.gettempdir(),
+      f'AutoPip_{sys.version_info.major}_{sys.version_info.minor}_site-packages'
+    )
+    os.makedirs(self.module_install_path, exist_ok=True)
+    sys.path.insert(0, self.module_install_path)
+
+  def find_spec(self, fullname, path, target=None):
+
+    if fullname in ['sitecustomize', 'usercustomize'] or '.' in fullname:
+      return None # Do not install system libs OR child libraries
+
+    if self.currently_recursing:
+      return None # Do not recurse infinitely
+
+    if fullname in sys.modules.keys():
+      return None # Skip already-imported modules
+
+    # Determine which line of code invoked us - if it's a folder under sys.path, always return None.
+    # Some python libs do a try/except to see what's available, and it's good to not
+    # dynamically install those libraries (like _decimal)
+    stack = traceback.extract_stack(limit=48)
+    line_called_from = traceback.format_list(stack)[0].split('\n')[1].strip()
+
+    # print(f'line_called_from = {line_called_from}')
+
+    if len(line_called_from) < 1 or not 'import ' in line_called_from.lower():
+      return None # Not called by an import statement!
+
+    # If there is a comment on the line, get all whitespace-deliminated words as tokens
+    # and use those as PIP arguments
+    pip_install_flags = []
+    if '#' in line_called_from:
+      comment_str = line_called_from.split('#', 1)[1].strip()
+      pip_install_flags = comment_str.split()
+
+    # Assume package name == module name and use that if no explicit comment found
+    if len(pip_install_flags) < 1:
+      pip_install_flags = [ fullname ]
+
+    # print(f'find_spec({fullname}, {path}, {target})')
+
+    self.currently_recursing = True
+
+    needs_install = False
+    try:
+      _ = importlib.import_module(fullname)
+    except:
+      needs_install = True
+
+    self.currently_recursing = False
+
+    if needs_install:
+      pip_env = os.environ.copy()
+      pip_env['PYTHONPATH'] = ''
+      subprocess.run([
+        sys.executable, '-m', 'pip', 'install',
+          f'--target={self.module_install_path}',
+          *pip_install_flags
+      ], env=pip_env, check=True)
+
+    return None
+
+
+sys.meta_path.insert(0, AutoPipMetaFinder())
+
+
+".Trim());
           return tmp_sitecustomize_py;
         }
         public static void Main(string[] args)
--- chaos.py
+++ chaos.py
@@ -5,19 +5,30 @@ print(f'json5 = {json5}')
 
 # From https://matplotlib.org/stable/gallery/lines_bars_and_markers/simple_plot.html
 # matplotlib is a good test of our "installer", as it comes with native code + calls executables on the PATH
-import matplotlib.pyplot as plt
-import numpy as np
+if False:
+  import matplotlib.pyplot as plt
+  import numpy as np
 
-# Data for plotting
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
+  print(f'plt = {plt}')
+  print(f'np = {np}')
 
-fig, ax = plt.subplots()
-ax.plot(t, s)
 
-ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
-ax.grid()
+  # Data for plotting
+  t = np.arange(0.0, 2.0, 0.01)
+  s = 1 + np.sin(2 * np.pi * t)
 
-fig.savefig("test.png")
-plt.show()
+  fig, ax = plt.subplots()
+  ax.plot(t, s)
+
+  ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+         title='About as simple as it gets, folks')
+  ax.grid()
+
+  fig.savefig("test.png")
+  plt.show()
+
+
+from PIL import Image # pillow
+
+print(f'PIL.Image = {Image}')
+
